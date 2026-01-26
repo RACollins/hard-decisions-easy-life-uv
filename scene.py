5b7f4192b51cec7ba170be82425ef319c9b0f4b9
RACollins <richardcollins128@gmail.com>
2026-01-27 06:58:57 +0900

    Refactor LifeSimulation class in scene.py to rename choice function for clarity, update generator type to 'four_choice', and implement choice spread array generation in LifeSimulationAnimation. Adjust simulation parameters in the delayed_gratification notebook for improved analysis and visualization.

--- scene.py
+++ scene.py
@@ -122,7 +122,7 @@ class LifeSimulation:
 
         return {"m1": m1, "m2": m2, "s1": s1, "s2": s2, "a1": a1, "a2": a2, "expr": y}
 
-    def generate_4_choice_function(self, form: str):
+    def generate_four_choice_function(self, form: str):
         x = sp.symbols("x")
 
         m1 = np.random.uniform(10, 40)
@@ -197,9 +197,9 @@ class LifeSimulation:
                 self.generate_opposites_choice_function(form=form)
                 for form in ["bad-good", "good-bad"] * (n_choices // 2)
             ]
-        elif genorator == "4":
+        elif genorator == "four_choice":
             functions = [
-                self.generate_4_choice_function(form=form)
+                self.generate_four_choice_function(form=form)
                 for form in ["bad-good", "good-bad", "good-good", "bad-bad"]
                 * (n_choices // 4)
             ]
@@ -258,6 +258,17 @@ class Config:
 
 
 class LifeSimulationAnimation(Scene):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def generate_choice_spread_array(self, n_choices: int):
+        start = -0.1
+        end = 0.1
+        xs = np.linspace(start, end, n_choices)
+        self.choice_spread_array = np.stack([np.array([i] * 100) for i in xs])
+        print(self.choice_spread_array.shape)
+        print(self.choice_spread_array.T)
+
     def construct(self):
         # Define simulation configs
         classic_config = Config(
@@ -267,7 +278,7 @@ class LifeSimulationAnimation(Scene):
             "opposites", 12, 150, [Person("Amy", 50), Person("Barry", 100)]
         )
         four_choice_config = Config(
-            "4", 12, 150, [Person("Amy", 50), Person("Barry", 100)]
+            "four_choice", 12, 150, [Person("Amy", 50), Person("Barry", 100)]
         )
         random_config = Config(
             "random", 12, 150, [Person("Amy", 5), Person("Barry", 10)]
@@ -281,22 +292,31 @@ class LifeSimulationAnimation(Scene):
 
         # Define simulation
         simulation = LifeSimulation(
-            configs["random"].people,
-            configs["random"].genorator,
-            configs["random"].n_choices,
+            configs["four_choice"].people,
+            configs["four_choice"].genorator,
+            configs["four_choice"].n_choices,
         )
 
         # Check example choice array
         choice_array = simulation.generate_choice_array(
-            genorator=configs["random"].genorator,
-            n_choices=configs["random"].n_choices,
+            genorator=configs["four_choice"].genorator,
+            n_choices=configs["four_choice"].n_choices,
         )
+        print(choice_array.shape)
         print(choice_array)
+
+        self.generate_choice_spread_array(configs["four_choice"].n_choices)
+        print(self.choice_spread_array.shape)
+        print(self.choice_spread_array)
+
+        choice_array_to_plot = choice_array + self.choice_spread_array
+        print(choice_array_to_plot.shape)
+        print(choice_array_to_plot)
 
         # Plot choice array
         ax = Axes(
             x_range=[0, 100],
-            y_range=[-0.01, 0.01],
+            y_range=[-0.1, 0.1],
         )
         # self.play(Create(ax)) # Uncomment to draw axes
 
